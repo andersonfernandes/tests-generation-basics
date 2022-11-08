0a32f27cf4104e3eefc228f13e67b689d3ea5eae
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-11-07 21:41:37 -0300

    Add mult and div
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,4 +6,14 @@ public class Calculator {
     public int sub(int x, int y) {
         return x - y;
     }
+
+    public int div(int x, int y) throws InvalidOperation {
+        if (y == 0) throw new InvalidOperation("Cannot div by zero");
+
+        return x / y;
+    }
+
+    public int mult(int x, int y) {
+        return x * y;
+    }
 }
