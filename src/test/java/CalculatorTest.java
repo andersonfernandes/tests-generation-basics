b6eb4ad90a4d7fa91ab4483f51de6960996e0302
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-11-07 21:55:18 -0300

    Update specs
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -24,5 +24,26 @@ public class CalculatorTest {
         assertEquals(0, (new Calculator()).sub(3, 3));
         assertEquals(-2, (new Calculator()).sub(1, 3));
     }
+
+    /**
+     * Method under test: {@link Calculator#div(int, int)}
+     */
+    @Test
+    public void testDiv() throws InvalidOperation {
+        assertEquals(0, (new Calculator()).div(2, 3));
+        assertEquals(1, (new Calculator()).div(3, 3));
+        assertEquals(0, (new Calculator()).div(1, 3));
+    }
+
+    /**
+     * Method under test: {@link Calculator#mult(int, int)}
+     */
+    @Test
+    public void testMult() {
+        assertEquals(6, (new Calculator()).mult(2, 3));
+        assertEquals(1, (new Calculator()).mult(1, 1));
+        assertEquals(9, (new Calculator()).mult(3, 3));
+        assertEquals(3, (new Calculator()).mult(1, 3));
+    }
 }
 
